52eeea64d388cd5a78698f6e9f1e42660d77c8d9
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-02-17 18:19:37 -0800

    implement move validity checks

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -151,3 +151,36 @@ class GameState():
         if second_last_move is None:
             return False
         return self.last_move.is_pass and second_last_move.is_pass
+
+    def is_move_self_capture(self, player, move):
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(move.point)
+        return next_board.get_go_string(move.point).num_liberties == 0
+
+    @property
+    def situation(self):
+        return (self.next_player, self.board)
+
+    def does_move_violate_ko(self, player, move):
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        next_situation = (player.other, next_board)
+        past_state = self.previous_state
+        while past_state is not None:
+            if past_state.situation == next_situation:
+                return True
+            past_state = paste_state.previous_state
+        return False
+
+    def is_valid_move(self, move):
+        if self.is_over():
+            return False
+        if move.is_pass or move.is_resign:
+            return True
+        return (self.board.get(move.point) is None and
+                not self.is_move_self_capture(self.next_player, move) and
+                not self.does_move_violate_ko(self.next_player, move))
